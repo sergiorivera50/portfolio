d2fcfe20460e97aa029b7bfd1ec772cdc49c5107
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-04 16:42:20 +0100

    Added projects controllers

--- server/src/api/controllers/projects.controller.ts
+++ server/src/api/controllers/projects.controller.ts
@@ -1,5 +1,37 @@
+import { Request, Response } from 'express'
+import { DeleteResult } from 'mongodb'
+import { Query } from 'mongoose'
+import ProjectModel from "../models/project.model"
+import { errorResponse, successResponse } from '../utils/express.util'
+
 export default class ProjectsController {
-  static async apiGetProjects(req, res) {
-    res.send({ message: 'Hello World!' })
+  static async apiGetProjects(req: Request, res: Response) {
+    let projects = []
+    try {
+      projects = await ProjectModel.find()
+    } catch (e) {
+      return errorResponse(res, "Unable to retrieve projects")
+    }
+    return successResponse(res, { projects: projects })
+  }
+
+  static async apiAddProject(req: Request, res: Response) {
+    const newProject = new ProjectModel({
+      title: req.body.title,
+      frameworks: req.body.frameworks
+    })
+    await newProject.save()
+    return successResponse(res, { created: newProject })
+  }
+
+  static async apiDeleteProject(req: Request, res: Response) {
+    const id = req.query.id
+    try {
+      const result = await ProjectModel.deleteOne({ _id: id })
+      if (result.deletedCount === 0) throw new Error("No documents deleted")
+    } catch (e) {
+      return errorResponse(res, e.message)
+    }
+    return successResponse(res, { deleted: id })
   }
 }

50ab76ee67aeb04abb607449f6b11566857902db
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-11 17:10:30 +0100

    Added loading timeout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
+import React, { useState, useEffect } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/global.css'
 import LoadingScreen from './components/LoadingScreen';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 7000)
+  })
+
   return (
     <>
-      <LoadingScreen />
+      { loading ? (
+        <LoadingScreen />
+      ) : (
+        <p>Dashboard</p>
+      )}
     </>
   );
 }

318ad2627c5a452525371922b186b092142fb2ec
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-11 15:57:41 +0100

    Animations rename/refactor

--- client/src/components/MainHeader.tsx
+++ client/src/components/MainHeader.tsx
@@ -12,9 +12,15 @@ class MainHeader extends React.Component {
           style={{ minHeight: "80vh" }}
         >
           <div>
-            <p style={{ fontWeight: "bold", fontSize: "2em" }}>Hi <span className="wave">👋</span> I am</p>
-            <h1 className="fadein duration2s" style={{ fontWeight: "bold", fontSize: "5em" }}>Sergio Rivera</h1>
-            <p className="fadein delay2s" style={{ fontWeight: "bold", fontSize: "2em", marginTop: "30px" }}>Welcome to my Project Tracker</p>
+            <p className="fadeinAnimation duration2s" style={{ fontWeight: "bold", fontSize: "2em" }}>
+              Hi <span className="waveAnimation">👋</span> I am
+            </p>
+            <h1 className="fadeinAnimation duration2s delay1s" style={{ fontWeight: "bold", fontSize: "5em" }}>
+              Sergio Rivera
+            </h1>
+            <p className="fadeinAnimation delay2s" style={{ fontWeight: "bold", fontSize: "2em", marginTop: "30px" }}>
+              Welcome to my Project Tracker
+            </p>
           </div>
         </Row>
       </Container>

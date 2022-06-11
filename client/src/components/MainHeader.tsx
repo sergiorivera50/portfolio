2628037f24f5eb67d54d56272d9d14161905d000
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-11 15:51:05 +0100

    Animations

--- client/src/components/MainHeader.tsx
+++ client/src/components/MainHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Container, Row } from "react-bootstrap"
 import "../css/debug.css"
+import "../css/animations.css"
 
 class MainHeader extends React.Component {
   render() {
@@ -11,9 +12,9 @@ class MainHeader extends React.Component {
           style={{ minHeight: "80vh" }}
         >
           <div>
-            <p style={{ fontWeight: "bold", fontSize: "4vh" }}>I am</p>
-            <h1 style={{ fontWeight: "bold", fontSize: "10vh" }}>Sergio Rivera</h1>
-            <p style={{ fontWeight: "bold", fontSize: "4vh", marginTop: "30px" }}>Welcome to my portfolio</p>
+            <p style={{ fontWeight: "bold", fontSize: "2em" }}>Hi <span className="wave">👋</span> I am</p>
+            <h1 className="fadein duration2s" style={{ fontWeight: "bold", fontSize: "5em" }}>Sergio Rivera</h1>
+            <p className="fadein delay2s" style={{ fontWeight: "bold", fontSize: "2em", marginTop: "30px" }}>Welcome to my Project Tracker</p>
           </div>
         </Row>
       </Container>

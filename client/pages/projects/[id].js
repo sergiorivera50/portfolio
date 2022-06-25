2bfe07348459c06a1f5947a31e2f9a488e2fe220
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-25 21:43:54 +0100

    Removed index in projects path

--- client/pages/projects/[id].js
+++ client/pages/projects/[id].js
@@ -14,7 +14,7 @@ const Project = ({ project }) => {
   return (
     <div className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-        {project.shortTitle}
+        {project._id}
       </div>
     </div>
   )

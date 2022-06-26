e05458a33c1e2d7778ce847b86c23d340144afd0
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-26 19:47:47 +0100

    Transparent gradient on project detail

--- client/pages/projects/[id].js
+++ client/pages/projects/[id].js
@@ -21,12 +21,8 @@ const Project = ({ project }) => {
   return (
     <div className='w-full'>
       <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/50 z-10' />
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-gradient-to-b from-transparent to-[#ecf0f3] opacity-50 z-10' />
           <Image className='absolute z-1' layout='fill' objectFit='cover' src={`data:image/png;base64,${project.thumbnail}`} alt='/' />
-          <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-            <h2 className='py-2'>{project.title}</h2>
-            <h3>{project.description}</h3>
-          </div>
       </div>
 
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>

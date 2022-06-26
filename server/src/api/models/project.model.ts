93febac75a27c9fa3f2a7b41e9f1afbe0d9cd571
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-26 17:09:45 +0100

    Simplified the Project model

--- server/src/api/models/project.model.ts
+++ server/src/api/models/project.model.ts
@@ -1,23 +1,13 @@
 import { Schema, model } from 'mongoose'
 
-const Resource = new Schema({
-  name: { type: String, required: true },
-  url:  { type: String, required: true },
-  icon: { type: String }
-})
-
 const ProjectSchema = new Schema({
   creationDate:     { type: Date,   default:  Date.now },  // date when the project was started
-  shortTitle:       { type: String, required: true     },  // really short title
-  fullTitle:        { type: String, required: true     },  // long title
-  shortDescription: { type: String, required: true     },  // really short description
-  fullDescription:  { type: String, required: true     },  // long description
+  title:            { type: String, required: true     },  // a title for the project
+  description:      { type: String, required: true     },  // a short description of the project
   thumbnail:        { type: String, required: true     },  // thumbnail url
   markdown:         { type: String, required: true     },  // full content in markdown
-  status:           { type: String, required: true     },  // current status of the project
-  tags:             { type: [String]                   },  // technologies, frameworks, techniques, ...
-  resources:        { type: [Resource]                 },  // urls pointing to external resources
-  featured:         { type: Boolean, default: false    },  // top 4 projects
+  technologies:     { type: [String]                   },  // technologies or frameworks used
+  featured:         { type: Boolean, default: false    },  // is it among the top 4 projects
 })
 
 const ProjectModel = model('Project', ProjectSchema)

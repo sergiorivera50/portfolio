5ae74ef8a2845359d84c2853d0bc4361d637c22a
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-26 16:58:36 +0100

    Use map instead of hardcoded

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,19 +1,25 @@
-import Image from 'next/image'
 import React from 'react'
 import githubCloneImg from '../public/assets/projects/project.png'
 import ProjectItem from './ProjectItem'
 
-const Projects = () => {
+const Projects = ({ projects }) => {
   return (
     <div id='projects' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
         <h2 className='py-4'>What I have built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-          <ProjectItem title='GitHub Clone' bgImg={githubCloneImg} projectPath='/projects/62b761a1d648424fe449717d' />
-          <ProjectItem title='GitHub Clone' bgImg={githubCloneImg} projectPath='/projects/62b761a1d648424fe449717d' />
-          <ProjectItem title='GitHub Clone' bgImg={githubCloneImg} projectPath='/projects/62b761a1d648424fe449717d' />
-          <ProjectItem title='GitHub Clone' bgImg={githubCloneImg} projectPath='/projects/62b761a1d648424fe449717d' />
+          {
+            projects.map((project, ix) => (
+              <ProjectItem 
+                key={ix}
+                title={project.shortTitle} 
+                description={project.shortDescription} 
+                bgImg={githubCloneImg} 
+                projectPath={`/projects/${project._id}`} 
+              />
+            ))
+          }
         </div>
       </div>
     </div>

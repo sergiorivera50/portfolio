1b5b1f0fb5865b3f5e77fb4b34fd4543ea184df4
sergiorivera50 <sriveralopez50@gmail.com>
2022-06-30 17:09:12 +0100

    Improved responsiveness of projects grid

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
+import { FiCornerRightDown } from 'react-icons/fi'
 
 const Projects = ({ projects }) => {
   return (
-    <div id='projects' className='w-full'>
+    <div id='projects' className='w-full pt-[40px]'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
-        <h2 className='py-4'>What I have built</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
+        <div className='flex justify-between items-center'>
+          <h2 className='py-2'>What I have built</h2>
+          <div className='flex justify-end pt-6'>
+            <p className='text-gray-500 mr-2'>hover over to see more</p>
+            <FiCornerRightDown className='mt-2' color='gray'/>
+          </div>
+        </div>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3'>
           {
             projects.map((project, ix) => (
               <ProjectItem 

c603c40559d1f245a253a54e1f563217a48f8d0e
Sergio Rivera <sriveralopez50@gmail.com>
2022-10-01 21:56:13 +0100

    Minor tweak of body image in About section

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -36,10 +36,11 @@ const About = () => {
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 flex items-center justify-center hover:scale-105 ease-in duration-300'>
           <Image 
             src={bodyImg}
             alt='/'
+            priority
           />
         </div>
       </div>

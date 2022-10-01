6fafaf5c6798b8476b61c8bb275fa866afd6901b
Sergio Rivera <sriveralopez50@gmail.com>
2022-10-01 19:34:01 +0100

    Import backend domain from env var

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,8 @@
-const { BACKEND_DOMAIN } = require('./http')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [ BACKEND_DOMAIN ]
+    domains: [ process.env.BACKEND_DOMAIN ]
   }
 }
 

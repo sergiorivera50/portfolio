b7fd5071f0d831de1be8cbcc47e47c2e1907a1a2
Sergio Rivera <sriveralopez50@gmail.com>
2022-10-12 12:22:36 +0100

    Fixed link hierarchy

--- client/components/ProjectItem.jsx
+++ client/components/ProjectItem.jsx
@@ -12,13 +12,13 @@ const ProjectItem = ({ title, imgSrc, description, projectPath }) => {
         <div className='hidden group-hover:block absolute w-[90%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
           <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
           <p className='pb-4 pt-2 text-white text-center'>{description}</p>
-            <Link href={projectPath}>
+          <Link href={projectPath}>
+            <div className='relative w-40 left-[50%] translate-x-[-50%]'>
               <a onClick={() => setLoadTriggered(true)} style={{textDecoration: 'none'}}>
-                <div className='relative w-40 left-[50%] translate-x-[-50%]'>
-                  <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More info</p>
-                </div>
+                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More info</p>
               </a>
-            </Link>
+            </div>
+          </Link>
         </div>
       </div>
     </LoadingOverlay>

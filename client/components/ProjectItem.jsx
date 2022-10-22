15a4f3c99ae3925c5e08030a0c3401260951bbc0
Sergio Rivera <sriveralopez50@gmail.com>
2022-10-22 04:00:08 +0100

    Refactored ProjectItem

--- client/components/ProjectItem.jsx
+++ client/components/ProjectItem.jsx
@@ -3,8 +3,20 @@ import Image from 'next/image'
 import Link from 'next/link'
 import LoadingOverlay from 'react-loading-overlay'
 
-const ProjectItem = ({ title, imgSrc, description, projectPath }) => {
+const ProjectItem = ({ project, placeholder }) => {
+
+  if (placeholder) {
+    return (
+      <div className='relative flex h-80 w-full bg-gray-300 shadow-xl shadow-gray-300 rounded-xl group'>
+        <div className='load-wraper'>
+          <div class="activity" />
+        </div>
+      </div>
+    )
+  }
+
   const [loadTriggered, setLoadTriggered] = useState(false)
+
   return (
     <LoadingOverlay spinner
       active={loadTriggered} 
@@ -28,11 +40,11 @@ const ProjectItem = ({ title, imgSrc, description, projectPath }) => {
       { loadTriggered ? <div className='h-80 w-full' />
         : (
           <div className='relative flex items-center justify-center h-80 w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff] hover:scale-[98%] ease-in duration-200'>
-            <Image className='rounded-xl group-hover:opacity-0' layout='fill' objectFit='cover' src={imgSrc} alt='/' priority />
+            <Image className='rounded-xl group-hover:opacity-0' layout='fill' objectFit='cover' src={project.thumbnail} alt='/' priority />
             <div className='hidden group-hover:block absolute w-[90%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-              <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
-              <p className='pb-4 pt-2 text-white text-center'>{description}</p>
-              <Link href={projectPath}>
+              <h3 className='text-2xl text-white tracking-wider text-center'>{project.title}</h3>
+              <p className='pb-4 pt-2 text-white text-center'>{project.description}</p>
+              <Link href={`/projects/${project._id}`}>
                 <div className='relative w-40 left-[50%] translate-x-[-50%]'>
                   <a onClick={() => setLoadTriggered(true)} style={{textDecoration: 'none'}}>
                     <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More info</p>

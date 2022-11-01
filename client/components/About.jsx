1ca70abd2f2e37b691554dbc46c708e2ca2ec100
Sergio Rivera <sriveralopez50@gmail.com>
2022-11-01 00:27:27 +0000

    Update about content

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -12,24 +12,22 @@ const About = () => {
           <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
           <h2 className='py-4'>A little bit about me</h2>
           <p className='py-2 text-gray-400 text-justify'>
-            My name is Sergio, I'm a software developer currently residing in the United Kingdom 🇬🇧
+            My name is Sergio. I'm a software developer currently residing in the United Kingdom 🇬🇧
           </p>
           <p className='py-2 text-gray-600 text-justify'>
-            Driven by passion and diligence, I've managed to endeavor on many areas of the Computer Science landscape
-            while maintaining a career focus on Artifical Intelligence and Data Science.
+          Driven by passion and diligence, I've managed to endeavour in many areas of the Computer Science landscape 
+          while maintaining a career focus on Artificial Intelligence and Data Science.
           </p>
           <p className='py-2 text-gray-600 text-justify'>  {/* Mention NLP work in IBM, polish The Platform */}
-            My most recent professional experience has been a 13-month internship with <strong>IBM</strong> at Hursley 
+            My most recent professional experience has been a 13-month internship with <strong>IBM</strong> at Hursley, 
             where I had the privilege to work alongside brilliant minds leveraging technologies such as Kubernetes, Jenkins 
-            and the Elastic Stack for the centralisation of logging and monitoring pipelines. It was during my placement when
+            and the Elastic Stack for the centralisation of logging and monitoring pipelines. During my placement, 
             I co-founded <strong>The Platform</strong>, a new form of online publishing heavily influenced by the Web3 ecosystem, where
             I created many of the open-source tools that our clients now use as well as various internal APIs and the automation of our infrastructure.
           </p>
           <p className='py-2 text-gray-600 text-justify'>
-            At the moment, I'm working on my final year project at <strong>Swansea University</strong> regarding the use of
-            conditional Generative Adversarial Networks for the generation of intermediate slices of three-dimensional datasets.
-            Such idea comes back from a small exploratory project I created on the summer of 2019, right before starting university,
-            where I experimented with the concept of increasing the resolution of CT scans through artificially generated slices. 
+            Currently, I'm working on my final year project at <strong>Swansea University</strong> regarding the use of
+            Generative Adversarial Networks for the generation of synthetic datasets.
             I expect to graduate on 2023 with a first-class (with honours) in Computer Science.
           </p>
           {/* <Link href='/#projects'>
